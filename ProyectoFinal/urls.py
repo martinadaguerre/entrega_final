0b7698941b5c0b96cfe15e12d59bb4d37dc15232
martinadaguerre <102626458+martinadaguerre@users.noreply.github.com>
2022-05-11 17:49:17 -0300

    saco el appfinal del path

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Appfinal/', include('Appfinal.urls'))   
+    path('/', include('Appfinal.urls'))   
 
 
 ]
